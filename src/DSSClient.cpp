222e44f70ff5ea9ff16e4780c521564ed9836b2c
Kienan Knight-Boehm <kienankb@gmail.com>
2015-07-21 22:33:08 -0400

    HEY WATCH WHAT YOU PUT IN YOUR HEADER FILES DUMMY

---
+++ src/DSSClient.cpp
@@ -0,0 +1,27 @@
+#include "DSSClient.h"
+
+//
+// Default constructor, we'll zero everything out
+//
+DSSClient::DSSClient() {
+	mSocket = 0;
+	mBufferSize = 0;
+	mAddress = NULL;
+	mPort = 0;
+	mDomain = 0;
+	mType = 0;
+	mProtocol = 0;
+}
+
+//
+// Simplest constructor; takes in an address and a port, but
+// otherwise defaults to internet, TCP, and some others
+//
+DSSClient::DSSClient(char* address, unsigned short port) {
+	mAddress = address;
+	mPort = port;
+	mDomain = AF_INET;
+	mType = SOCK_STREAM;
+	mProtocol = 0;
+	mBufferSize = 1024;
+}
--- src/DSSClient.h
+++ src/DSSClient.h
@@ -3,6 +3,8 @@
 
 class DSSClient {
 public:
+	DSSClient::DSSClient();
+	DSSClient::DSSClient(char* address, unsigned short port);
 private:
 	int mSocket;
 	int mBufferSize;
@@ -13,29 +15,3 @@ private:
 	int mType;
 	int mProtocol;
 };
-
-//
-// Default constructor, we'll zero everything out
-//
-DSSClient::DSSClient() {
-	mSocket = 0;
-	mBufferSize = 0;
-	mAddress = NULL;
-	mPort = 0;
-	mDomain = 0;
-	mType = 0;
-	mProtocol = 0;
-}
-
-//
-// Simplest constructor; takes in an address and a port, but
-// otherwise defaults to internet, TCP, and some others
-//
-DSSClient::DSSClient(char* address, unsigned short port) {
-	mAddress = address;
-	mPort = port;
-	mDomain = AF_INET;
-	mType = SOCK_STREAM;
-	mProtocol = 0;
-	mBufferSize = 1024;
-}
